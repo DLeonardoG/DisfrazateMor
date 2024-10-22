cc35f3cce5e90d51373dfa5ad9bf8fbc5864066e
ErikSneyPlata <eplata391@gmail.com>
2024-10-22 13:23:49 -0500

    feat: :art: Se inician los procedimientos de consultas avanzadas
    Se inician los procedimientos de consultas avanzadas con procedure
    
    by @ErikSneyPlata

---
+++ dql_procedimientos.sql
@@ -0,0 +1,20 @@
+-- 2. Registrar un nuevo proveedor: Un procedimiento para agregar información de nuevos proveedores a la base de datos.
+
+
+DELIMITER $$
+create procedure agragar_nuevo_proveedor(
+    nombre varchar(125),
+    celular char(10),
+    id_tipo_proveedor int
+)
+begin
+ -- declaraciones SQL y logica de procediento 
+	insert  into proveedores (nombre, celular, id_tipo_proveedor)
+    values (nombre, celular, id_tipo_proveedor);
+    
+end $$
+DELIMITER ;
+
+call agragar_nuevo_proveedor('Paisas la 36', '3123404945', 2);
+
+select * from proveedores;
